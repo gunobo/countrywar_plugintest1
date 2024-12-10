f79aa78122dc6d2dea92039b71fa172936a8290b
gunobo <113815207+gunobo@users.noreply.github.com>
2024-12-11 08:00:43 +0900

    Update MobSpawnListener.java

--- src/main/java/com/arckenver/nations/listener/MobSpawnListener.java
+++ src/main/java/com/arckenver/nations/listener/MobSpawnListener.java
@@ -8,9 +8,12 @@ public class MobSpawnListener implements Listener {
 
     @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent event) {
-        // 적대적 몹과 중립적 몹의 스폰을 차단
+        // 적대적 몹, 중립적 몹, 발광 오징어, 박쥐, 팬텀의 스폰을 차단
         if (event.getEntity() instanceof org.bukkit.entity.Monster || 
-            event.getEntity() instanceof org.bukkit.entity.Animal) {
+            event.getEntity() instanceof org.bukkit.entity.Animal || 
+            event.getEntity() instanceof org.bukkit.entity.GlowingSquid || 
+            event.getEntity() instanceof org.bukkit.entity.Bat || 
+            event.getEntity() instanceof org.bukkit.entity.Phantom) {
             event.setCancelled(true);
         }
     }
